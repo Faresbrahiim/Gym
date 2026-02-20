44465757bd5fb0cea683409d919be5ee8dd3cd50
ifares <hmsteven37@gmail.com>
2026-02-20 16:15:40 +0000

    update/add env.example

--- microservices/user-service/obj/Debug/net9.0/user-service.AssemblyInfo.cs
+++ microservices/user-service/obj/Debug/net9.0/user-service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("user-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd48bcf8385d015efdf51b54593e0c958404bf0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5377e4a3c204e81af98a19f82a59d63bbc58645")]
 [assembly: System.Reflection.AssemblyProductAttribute("user-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("user-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
